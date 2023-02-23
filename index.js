41d82f9cccebecd81666c4baf01f7bd8148443eb
jgeorgilas <justin.georgilas@gmail.com>
2023-02-22 21:54:12 -0800

    added connected wires to ride power

--- index.js
+++ index.js
@@ -11,21 +11,33 @@ let waterIsOn = true
 
 //Ride 1 works when:
     // green wire is connected
-ride1PowerOn = false
+
+    if (greenWireConnected){
+        ride1PowerOn = true;
+    }
 
 //Ride 2 works when 
     //green and yellow wires are connected
-ride2PowerOn = false
+
+    if (greenWireConnected && yellowWireConnected){
+        ride2PowerOn = true;
+    }
 
 //Ride 3 works when:
     // green and red wires are connected
     // water is on 
     // yellow wire is not connected
-ride3PowerOn = false
 
+    if (greenWireConnected && redWireConnected && waterIsOn && yellowWireConnected){
+    ride3PowerOn = true;
+    }
+    
 //Ride 4 has power when:
     //ride wire is connected
-ride4PowerOn = false
+    if (redWireConnected){
+        ride4PowerOn = true;
+    }
+
 //Ride 4 is a slot machine
     //if a 1 is rolled it should return "star"
     //if a 2 is rolled it should return "car"
@@ -34,6 +46,9 @@ ride4PowerOn = false
     //if a 5 is rolled it should return "bowling ball"
     //if a 6 is rolled it should return "tiger"
     //if a 7 is rolled it should return "seven"
-function ride4SlotMachine(rolledNumber){
-    return "nothing"
+
+    function ride4SlotMachine(rolledNumber){
+    if (rolledNumber = 3){
+        return "star"
+    }
 }
